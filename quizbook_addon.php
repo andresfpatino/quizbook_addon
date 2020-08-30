6b6dd65ace0da49067735367264d55c8a25e0d6a
Andrés Patiño <andresfelipepatino5@gmail.com>
2020-08-30 13:35:42 -0500

    Creando el postype de examenes

--- quizbook_addon.php
+++ quizbook_addon.php
@@ -11,8 +11,10 @@
     Text Domain: quizbook
 */
 
-function quizbook_examen_revisar() {
-    // Verificamos que exista una función del plugin principal, si no lanzamos el error
+if(! defined('ABSPATH')) exit; // evita la lectura por url
+
+// Verificamos que exista una función del plugin principal, si no lanzamos el error
+function quizbook_examen_revisar() {    
     if(!function_exists('quizbook_post_type')) { 
         add_action('admin_notices', 'quizbook_addon_error_activar');
         deactivate_plugins(plugin_basename(__FILE__));
@@ -21,9 +23,14 @@ function quizbook_examen_revisar() {
 add_action('admin_init', 'quizbook_examen_revisar');
 
 
-// Mensaje de error
+// Mensaje de error en caso no tener el plugin principal
 function quizbook_addon_error_activar(){
     $clase = 'notice notice-error';
     $mensaje = "Un error ocurrió, necesitas instalar el Plugin principal 'Quizbook'.";
     printf('<div class=" %1$s "> <p> %2$s Puedes encontrarlo <a target="_blank" href="https://github.com/andresfpatino/quizbook"> Aquí </a> </p> </div>', esc_attr($clase), esc_html($mensaje) );
 }
+
+
+// Crea el postype de examenes
+require_once plugin_dir_path(__FILE__) . 'includes/posttypes.php';
+register_activation_hook(__FILE__, 'quizbook_examenes_rewrite_flush'); 

9706a552dabceab5c44e6246d5c9935f5c4fffa3
Andrés Patiño <andresfelipepatino5@gmail.com>
2020-08-30 11:58:58 -0500

    Inizialización

---
+++ quizbook_addon.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin Name: Quizbook - Addon
+    Plugin URI:
+    Description: Versión premiunm del plugin Quizbook. Esta versión te permite crear examenes para tus Quizbook
+    Version: 1.0
+    Author: Andrés Patiño
+    Author URI: mailto:andresfelipepatino5@gmail.com
+    License: GPL2
+    License URI: https://www.gnu.org/licences/gpl-2.0.html
+    Text Domain: quizbook
+*/

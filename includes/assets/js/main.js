ce2b5b194802cf88ccc6ecc5ae555dc1c3fe2a63
Andrés Patiño <andresfelipepatino5@gmail.com>
2020-09-16 20:44:55 -0500

    Add chosen to metaboxes

--- includes/assets/js/main.js
+++ includes/assets/js/main.js
@@ -1,3 +1,3 @@
 (function($){
-    alert('Hola');
+   $('.preguntas_select').chosen();
 })(jQuery);
--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -9,6 +9,42 @@ add_action('add_meta_boxes', 'quizbook_examen_agregar_metaboxes');
 
 function quizbook_examen_metaboxes(){ ?>
    
+	<table class="form-table">
+		<tr>
+			<th class="row-title" colspan="2"></th>
+			<h2>Selecciona las respuestas para que se incluyan en este examen</h2>
+		</tr>
+		<tr>
+			<th class="row-title"> <label for=""> Selecciona de la lista </label> </th>
+			<td>
+				<?php 
+					$args = array(
+						'post_type'	=> 'quizes',
+						'posts_per_page' => -1, 
+					);
+
+					$preguntas = get_posts($args);
+
+					if($preguntas): ?>
+
+						<select data-placeholder="Selecciona las preguntas" class="preguntas_select" multiple tabindex="4">
+							<option value=""></option>
+							<?php foreach ($preguntas as $pregunta): ?>
+								<option value="<?php echo  $pregunta->ID; ?>"> <?php echo $pregunta->post_title; ?> </option>
+							<?php endforeach; ?>
+						</select>
+
+					<?php else:
+
+						echo '<p> Comienza por agregar preguntas en Quiz </p>';
+
+					endif;
+				?>
+			
+			
+			</td>
+		</tr>
+   	</table>
 
 <?php 
 }

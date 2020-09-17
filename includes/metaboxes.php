c81c361f5ba72c75f45d1281643563b705be101b
Andrés Patiño <andresfelipepatino5@gmail.com>
2020-09-16 21:04:14 -0500

    Guardando preguntas del select

--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -7,7 +7,11 @@ function quizbook_examen_agregar_metaboxes(){
 }
 add_action('add_meta_boxes', 'quizbook_examen_agregar_metaboxes');
 
-function quizbook_examen_metaboxes(){ ?>
+function quizbook_examen_metaboxes(){ 
+	
+	wp_nonce_field(basename(__FILE__), 'quizbook_examen_nonce');
+	
+	?>
    
 	<table class="form-table">
 		<tr>
@@ -27,7 +31,7 @@ function quizbook_examen_metaboxes(){ ?>
 
 					if($preguntas): ?>
 
-						<select data-placeholder="Selecciona las preguntas" class="preguntas_select" multiple tabindex="4">
+						<select data-placeholder="Selecciona las preguntas" name="quizbook_examen[]" class="preguntas_select" multiple tabindex="4">
 							<option value=""></option>
 							<?php foreach ($preguntas as $pregunta): ?>
 								<option value="<?php echo  $pregunta->ID; ?>"> <?php echo $pregunta->post_title; ?> </option>
@@ -48,3 +52,31 @@ function quizbook_examen_metaboxes(){ ?>
 
 <?php 
 }
+
+
+/* Guarda la Info de los Metaboxes */
+function quizbook_examen_guardar_metaboxes($post_id, $post, $update) {
+
+    if(!isset($_POST['quizbook_examen_nonce']) || !wp_verify_nonce( $_POST['quizbook_examen_nonce'], basename(__FILE__)  ) ) // verifica el nonce creado arriba
+    return $post_id;
+
+    if(!current_user_can('edit_post', $post_id)) // verifica permisos de edicion
+    return $post_id;
+
+    if(defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+	return $post_id;
+
+	$respuestas = '';
+	$arreglo_respuestas = array();
+
+	if(isset($_POST['quizbook_examen'])) {
+		$respuestas = $_POST['quizbook_examen'];
+		
+		foreach ($respuestas as $respuesta): 
+			$arreglo_respuestas[] = sanitize_text_field( $respuesta );
+		endforeach;
+	}
+
+	update_post_meta($post_id, 'quizbook_examen', maybe_serialize( $arreglo_respuestas ) );
+}
+add_action('save_post', 'quizbook_examen_guardar_metaboxes', 10, 3); // 10 = prioridad 3 = cantidad parametros $post_id, $post, $update 
